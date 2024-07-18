dfc7f1d5baf2b212154f2fb58b03ded1573c02d4
SAKTHI MEENAKSHI ANANDAN <85392143+Meenakshi-here@users.noreply.github.com>
2024-07-18 22:51:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI() #application app as object/instance
 db:List[User] =[
     User(
          id=UUID("50ecc8e8-64c2-40e5-95ab-ab4ee5740e49"),
-         first_name="Sakthi Meenakshi",
+         first_name="Tulip",
          last_name = "A",
          gender = Gender.female,
          roles = [Role.student]
@@ -18,7 +18,7 @@ db:List[User] =[
          ),
      User(
          id=UUID("7b13a673-361a-4aca-ab44-a94fb8d8e73e"), 
-         first_name="Vishwa",
+         first_name="Jasmine",
          last_name = "S",
          gender = Gender.male,
          roles = [Role.student]
